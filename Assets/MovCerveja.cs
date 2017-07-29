95e10c6762347af3f366019c226ef6192a412930
Iago Lobo <iago.wolf.moraes@gmail.com>
2017-07-29 15:23:48 -0300

    Feito movimento oscilatório do chopp

---
+++ Assets/MovCerveja.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovCerveja : MonoBehaviour
+{
+    public float sinCounter;
+    public float sinCounterStep;
+    public float shakeAmplitude;
+
+    void FixedUpdate()
+    {
+        sinCounter += sinCounterStep;
+        gameObject.transform.Translate(new Vector2(0, Mathf.Sin(sinCounter)* shakeAmplitude));
+    }
+}
