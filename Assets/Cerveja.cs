55075414a6cc831674a7629e162b38188e5263cf
Iago Lobo <iago.wolf.moraes@gmail.com>
2017-07-30 01:56:07 -0300

    Scrolling e Parallax implementados
    Não funcionam com o personagem, e sim com a câmera apenas.

--- Assets/Cerveja.cs
+++ Assets/Cerveja.cs
@@ -13,7 +13,7 @@ public class Cerveja : MonoBehaviour
 
     private void Start()
     {
-        player = GameObject.Find("Person");
+        player = GameObject.Find("Player");
         if (null == player) {
             print("Oops");
         }
